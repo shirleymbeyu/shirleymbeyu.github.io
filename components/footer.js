fc7369869cd8712590053f2009da1fc9b52794ef
Shirley Ddaiddo <mbeyushirley@gmail.com>
2025-07-12 18:53:02 +0200

    Homepage,education and footer error correction

--- components/footer.js
+++ components/footer.js
@@ -10,8 +10,8 @@ class Footer extends HTMLElement {
         <div class="py-3 px-md-5 container-fluid">
             <div class="row">
                 <div class="col-auto col-md-5 align-middle">
-                    <p class="mb-0" style="color: var(--light-gray);">© 2024 Your Name</p>
-                    <p class="my-0 pt-0"><a class="" href="mailto:yourname@email.org">yourname@email.org</a></p>
+                    <p class="mb-0" style="color: var(--light-gray);">© 2025 Shirley Mbeyu Ddaiddo</p>
+                    <p class="my-0 pt-0"><a class="" href="mailto:sddaiddo@andrew.cmu.edu">sddaiddo@andrew.cmu.edu</a></p>
 
                 </div>
 
@@ -19,11 +19,9 @@ class Footer extends HTMLElement {
                 <div class="col col-md-7 text-right">
                     <p class="mb-0" style="color: var(--light-gray);">Quick Links</p>
                     <p class="my-0 pt-0">
-                        <a href=""><i class="mr-3 footer-icons fa-solid fa-house" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 fab footer-icons fa-linkedin" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 footer-icons fa-brands fa-google-scholar" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 fab footer-icons fa-github" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="footer-icons fa-brands fa-x-twitter" aria-hidden="true"></i></a>
+                        <a href="https://shirleymbeyu.github.io/"><i class="mr-3 footer-icons fa-solid fa-house" aria-hidden="true"></i></a>
+                        <a href="www.linkedin.com/in/shirley-ddaiddo" target="_blank"><i class="mr-3 fab footer-icons fa-linkedin" aria-hidden="true"></i></a>
+                        <a href="https://github.com/shirleymbeyu" target="_blank"><i class="mr-3 fab footer-icons fa-github" aria-hidden="true"></i></a>
                     </p>
                 </div>
             </div>
