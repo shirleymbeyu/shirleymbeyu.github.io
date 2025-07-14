ce718104da8c8f37a8c81bc361d15f2665883d06
Shirley Ddaiddo <mbeyushirley@gmail.com>
2025-07-14 15:16:26 +0200

    experience

--- components/header.js
+++ components/header.js
@@ -7,7 +7,7 @@ class Header extends HTMLElement {
     this.innerHTML = `
       <header>
         <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
-        <h1><a class="navbar-brand" href="/">Your Name</a></h1>
+        <h1><a class="navbar-brand" href="/">Shirley Mbeyu Ddaiddo</a></h1>
         <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
